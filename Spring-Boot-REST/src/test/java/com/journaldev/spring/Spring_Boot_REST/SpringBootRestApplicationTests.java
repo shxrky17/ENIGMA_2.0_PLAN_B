f322f4d0f777a1d13928564498ce4729f153eac6
shxrky17 <144344740+shxrky17@users.noreply.github.com>
2026-02-26 19:41:37 +0530

    a

---
+++ Spring-Boot-REST/src/test/java/com/journaldev/spring/Spring_Boot_REST/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journaldev.spring.Spring_Boot_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
